3ec1bc1f965c009b2d8f02e4cb53b740ee5e7abe
Juan B <juan@crowdbotics.com>
2023-11-10 13:47:52 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,15 +1,14 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "juanprdfrom-4",
-  url: "https://juanprdfrom-4.botics.co",
-  api: "https://juanprdfrom-4.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
-}
+  "name": "juanprdfrom-4",
+  "url": "https://juanprdfrom-4.botics.co",
+  "api": "https://juanprdfrom-4.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "react_native_typeform_webhook"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
